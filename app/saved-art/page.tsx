19254307280989ef59608028a32f58e8d8902876
Asiya Batool <asiya.batool987@gmail.com>
2024-07-01 16:42:52 +0500

    spinner added

--- app/saved-art/page.tsx
+++ app/saved-art/page.tsx
@@ -5,71 +5,82 @@ import { useAuth } from "@clerk/nextjs";
 import { fetchUserImages } from "../firebase/fetchImages";
 import PhotoSwipeLightbox from "photoswipe/lightbox";
 import "photoswipe/style.css";
+import { ClipLoader } from "react-spinners";
 
 const SavedArtPage: React.FC = () => {
-  const [images, setImages] = useState<DocumentData[]>([]);
-  const { userId } = useAuth();
+    const [images, setImages] = useState<DocumentData[]>([]);
+    const { userId } = useAuth();
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const fetchImages = async () => {
+            if (!userId) {
+                setLoading(false);
+                return;
+            }
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      if (!userId) {
-        return;
-      }
+            try {
+                const fetchedImages = await fetchUserImages(userId);
+                console.log(fetchedImages, "images")
+                setImages(fetchedImages);
+            } catch (error) {
+                console.error("Error fetching images:", error);
+            }
+            finally {
+                setLoading(false);
+            }
+        };
 
-      try {
-        const fetchedImages = await fetchUserImages(userId);
-        console.log(fetchedImages,"images")
-        setImages(fetchedImages);
-      } catch (error) {
-        console.error("Error fetching images:", error);
-      }
-    };
+        fetchImages();
+    }, [userId]);
 
-    fetchImages();
-  }, [userId]);
-
-  function isPhonePortrait() {
-    return window.matchMedia('(max-width: 600px) and (orientation: portrait)').matches;
-  }
-  useEffect(() => {
-    if (images.length > 0) {
-      const lightbox = new PhotoSwipeLightbox({
-        gallery:'#gallery',
-        children:'a',
-        
-        pswpModule: () => import("photoswipe"),
-      });
-      lightbox.init();
-      return () => lightbox.destroy();
+    function isPhonePortrait() {
+        return window.matchMedia('(max-width: 600px) and (orientation: portrait)').matches;
     }
-  }, [images]);
+    useEffect(() => {
+        if (images.length > 0) {
+            const lightbox = new PhotoSwipeLightbox({
+                gallery: '#gallery',
+                children: 'a',
 
-  return (
-    <div className="min-h-screen flex justify-center items-start">
-      <div className="container mx-auto py-8">
-        <h1 className="text-3xl font-semibold text-center mb-4">Saved Art</h1>
-        <div id="gallery" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {images.map((image, index) => (
-            <a
-              key={index}
-              href={image.imageUrl}
-              data-pswp-width={300}
-              data-pswp-height={300}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block overflow-hidden rounded-lg shadow-lg object-cover"
-            >
-              <img
-                src={image.imageUrl}
-                alt={image.description}
-                className="object-cover w-full h-full"
-              />
-            </a>
-          ))}
+                pswpModule: () => import("photoswipe"),
+            });
+            lightbox.init();
+            return () => lightbox.destroy();
+        }
+    }, [images]);
+
+    return (
+        <div className="min-h-screen flex justify-center items-start">
+            <div className="container mx-auto py-8">
+                <h1 className="text-3xl font-semibold text-center mb-4">Saved Art</h1>
+                {loading ? (
+                    <div className="flex justify-center items-center h-64">
+                        <ClipLoader size={50} color={"#123abc"} loading={loading} />
+                    </div>
+                ) : (
+                    <div id="gallery" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                        {images.map((image, index) => (
+                            <a
+                                key={index}
+                                href={image.imageUrl}
+                                data-pswp-width={300}
+                                data-pswp-height={300}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="block overflow-hidden rounded-lg shadow-lg object-cover"
+                            >
+                                <img
+                                    src={image.imageUrl}
+                                    alt={image.description}
+                                    className="object-cover w-full h-full"
+                                />
+                            </a>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SavedArtPage;
